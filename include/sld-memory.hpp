f3dde0a92c359233500077ace065081f6a4403ae
Sam Davenport <sam.davenport@thereclaimergame.com>
2026-05-12 23:54:41 -0500

    working on convention

--- include/sld-memory.hpp
+++ include/sld-memory.hpp
@@ -22,18 +22,17 @@ namespace sld {
     struct arena;
 
     // stack memory
-    SLD_MEMORY_API u32              stack_allocator_memory_size (const u32 size, const u32 granularity);
-    SLD_MEMORY_API stack_allocator* stack_allocator_memory_init (const u32 size, const u32 granularity, void* memory);
-    SLD_MEMORY_API void*            stack_alloc                 (stack_allocator* alctr, const u32 size = 0, const u32 alignment = 0);
-    SLD_MEMORY_API void             stack_free                  (stack_allocator* alctr, void* memory);
+    SLD_MEMORY_API u32              stack_memory_size (const u32 size, const u32 granularity);
+    SLD_MEMORY_API stack_allocator* stack_memory_init (const u32 size, const u32 granularity, void* memory);
+    SLD_MEMORY_API void*            stack_alloc       (stack_allocator* alctr, const u32 size = 0, const u32 alignment = 0);
+    SLD_MEMORY_API void             stack_free        (stack_allocator* alctr, void* memory);
 
     // block memory
-    SLD_MEMORY_API u32              block_allocator_memory_size (const u32 block_count, const u32 block_size);
-    SLD_MEMORY_API block_allocator* block_allocator_memory_init (const u32 block_count, const u32 block_size);
-    SLD_MEMORY_API void*            block_alloc                 (block_allocator* alctr);
-    SLD_MEMORY_API void*            block_free                  (block_allocator* alctr);
+    SLD_MEMORY_API u32              block_memory_size (const u32 block_count, const u32 block_size);
+    SLD_MEMORY_API block_allocator* block_memory_init (const u32 block_count, const u32 block_size);
+    SLD_MEMORY_API void*            block_alloc       (block_allocator* alctr);
+    SLD_MEMORY_API void*            block_free        (block_allocator* alctr);
 
-    SLD_MEMORY_API u32             are
 };
 
 #endif //SLD_MEMORY_HPP
